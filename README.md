47d93312031693d836dd65d17248355531b33bca
smarties713 <odst.mrtn@gmail.com>
2019-12-06 00:03:38 -0600

    final push

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# weather-api
+* Application is made to be able to enter desiried city and find out what the weather is for that day.
+* Ideally, it should be able to display 5 days worth of information.
+* As of now only the following is able to be displayed:
+    * Temperature
+    * Humidity
+    * Wind Speed
+    * UV Index
+
+* Further iderations will allow for a full 5 day forecast of desired city as well as having previous searches saved and displayed to the left hand side of the          console. 
--- script.js
+++ script.js
@@ -1,30 +1,32 @@
+// Display current date
 var currentDay = moment().format('MMMM Do YYYY');
 $("#currentDay").text(currentDay);
 
-
+// On click, searches based on city or zip code.
 $("#search").click(function () {
     var location = $("#location").val();
-    weatherInfo(location)
+    weatherInfo(location);
 });
 
-
+// Accesses weather API data base
 function weatherInfo(location) {
-    var queryURL = "https://api.weatherbit.io/v2.0/forecast/daily?city=" + location + "&key=26ec32875e6342549b54ba0964115b05";
+    var queryURL = "https://api.weatherbit.io/v2.0/forecast/daily?city=" + location + "&key=26ec32875e6342549b54ba0964115b05&units=I";
     $.ajax({
         url: queryURL,
         method: "GET"
     }).then(function (responseWeatherInfo) {
         $("temperature")
-        console.log(responseWeatherInfo)
+        console.log(responseWeatherInfo);
+        // Loop through the first 5 arrays to display the next 5 days of the weather
         for (let i = 0; i < 5; i++) {
-            //let tempC = responseWeatherInfo.data[i]temp;
-            //let tempF = (9/5)*tempC + 32
-            $(".temperature").text("Temperature " + responseWeatherInfo.main.temp + " F")
-            $(".humidity").text("Humidity " + responseWeatherInfo.main.humidity + "%")
-            $(".windSpeed").text("Wind Speed " + responseWeatherInfo.wind.speed + "mph")
+            $(".temperature").text("Temperature: " + responseWeatherInfo.data[i].temp + " F");
+            $(".humidity").text("Humidity: " + responseWeatherInfo.data[i].rh + "%");
+            $(".windSpeed").text("Wind Speed: " + responseWeatherInfo.data[i].wind_spd + "mph");
+            $(".uv").text("UV Index: " + responseWeatherInfo.data[i].uv);
 
         }
-        // $.each(responseWeatherInfo.data[i],function() {
-        // })
+        // Print each day's weather
+        $.each(responseWeatherInfo.data[i],function() {
+        })
     })
 }
