3fc7e3091665ea728618556b5282ca1b461e5e72
MMunoz713 <odst.mrtn@gmail.com>
2019-12-05 21:49:22 -0600

    jquery added

--- script.js
+++ script.js
@@ -1,15 +1,30 @@
-var currentDay = moment().format('MMMM Do YYYY, h:mm:ss a');
+var currentDay = moment().format('MMMM Do YYYY');
 $("#currentDay").text(currentDay);
 
 
+$("#search").click(function () {
+    var location = $("#location").val();
+    weatherInfo(location)
+});
 
-function weatherInfo(){
-    var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + location + "&appid=0a87b047396d3f1e1b0d1c65b1f0e606";
-    
+
+function weatherInfo(location) {
+    var queryURL = "https://api.weatherbit.io/v2.0/forecast/daily?city=" + location + "&key=26ec32875e6342549b54ba0964115b05";
     $.ajax({
         url: queryURL,
         method: "GET"
-    }).then(function(response) {
+    }).then(function (responseWeatherInfo) {
         $("temperature")
-    }
+        console.log(responseWeatherInfo)
+        for (let i = 0; i < 5; i++) {
+            //let tempC = responseWeatherInfo.data[i]temp;
+            //let tempF = (9/5)*tempC + 32
+            $(".temperature").text("Temperature " + responseWeatherInfo.main.temp + " F")
+            $(".humidity").text("Humidity " + responseWeatherInfo.main.humidity + "%")
+            $(".windSpeed").text("Wind Speed " + responseWeatherInfo.wind.speed + "mph")
+
+        }
+        // $.each(responseWeatherInfo.data[i],function() {
+        // })
+    })
 }
